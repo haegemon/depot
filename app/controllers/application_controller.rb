1a87c6bc0596d31163f08f932265f5011a3fddcb
pawel <pawel.samysev@gmail.com>
2015-02-05 00:16:27 +0300

    finish part 15 not all trans

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     before_action :authorize
+    before_action :set_i18n_locale_from_params
 
     protected
 
     def authorize
         unless User.find_by(id: session[:user_id])
             redirect_to login_url, notice: "Please login"
+        end
+    end
+
+    def default_url_options
+        {locale: I18n.locale}
+    end
+
+    def set_i18n_locale_from_params
+        if params[:locale]
+            if I18n.available_locales.map(&:to_s).include?(params[:locale])
+                I18n.locale = params[:locale]
+            else
+                flash[:notice] = "Translation not available"
+                logger.error flash.now[:notice]
+            end
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,20 @@ Depot::Application.routes.draw do
 
   resources :users
 
-  resources :orders
 
-  resources :line_items
 
-  resources :carts
 
   get "store/index"
   resources :products do
       get :who_bought, on: :member
   end
+    scope '(:locale)' do
+        resources :orders
+        resources :line_items
+        resources :carts
+        root 'store#index', as: 'store', via: :all
+    end
 
-  root 'store#index', as: 'store'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
